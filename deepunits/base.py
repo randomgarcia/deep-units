8936da6e54f240fd4f63bc4b334ec7fe0c880789
AdamC <randomgarcia@gmail.com>
2021-04-06 18:33:52 +0100

    Creating a nested dict for storing tensors

--- deepunits/base.py
+++ deepunits/base.py
@@ -6,16 +6,34 @@ from tensorflow.keras.layers import (
     Activation,
 )
 
-class DeepUnit:
+from collections import OrderedDict
+
+class TensorDict:
+    """
+    Wrapper around an OrderedDict to make accessing saved tensors easier
+    """
     def __init__(self):
+        self.NestedDict = OrderedDict()
+    
+    def __getattr__(self,*args):
+        pass
+    
+
+class DeepUnit:
+    def __init__(self,units=None,names=None):
         
-        # could use an OrderedDict
+        # could use an OrderedDict?
         self.Units = {}
         self.Tensors = {}
         self.Names = []
     
     def __call__(self,x):
+        # do some checking here at some point 
+        return self.tf_call(x)
+    
+    def tf_call(self,x,replace=True):
         
+        self.Tensors['input'] = x
         z = x 
         for currname in self.Names:
             z = self.Units[currname](z)
